ecf5ff55bb32844c8c43b6cc3b6c3b5f9a9a7fdb
MShackford <93612680+MalloryShackford@users.noreply.github.com>
2025-01-02 14:15:54 -0500

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
      <div id="contact" className="contact-container">
         <h1 className="contact-h1">Contact</h1>
         <div className="form-container">
-         <form className="submit_form form" action="https://formsubmit.co/     eb8400d13809384a217392d8c97d16e" method="POST">
+         <form className="submit_form form" action="https://formsubmit.co/eb8400d13809384a217392d8c97d16e" method="POST">
            <input type="text" name="name" placeholder="Name" required />
            <input type="email" name="email" placeholder="Email" required />
            <input type="text" name="message" placeholder="Message" required />
